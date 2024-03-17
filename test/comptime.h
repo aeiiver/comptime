7013ec6fdb85d8c38245c66ae0b4b66170bfa432
aeiiver <aeiiver at proton dot me>
2024-03-17 10:30:42 +0100

    reset

--- test/comptime.h
+++
@@ -1,9 +0,0 @@
-int comptime_42(void);
-int comptime_add3(int, int, int);
-char comptime_get_second_char(char *);
-char comptime_get_last_char(char *);
-int comptime_strlen(char *);
-
-char *comptime_env(char *);
-void comptime_print_delayed(char *, int);
-void comptime_httpbin_postanything(char *, int);
